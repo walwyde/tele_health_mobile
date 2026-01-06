ee4f439d523b272718f846fb602d7e02ea442e53
Yobe Auwal <walwyde@gmail.com>
2026-01-06 22:51:24 +0100

    keys hidden

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 import 'screens/symptom_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  // Load environment variables
+  await dotenv.load(fileName: ".env");
+
   await Supabase.initialize(
-    url: 'https://niubbceotjbeiohcdxrx.supabase.co',
-    anonKey: 'sb_publishable_-HRcOxSZ20_HK7xD2RhKVA_HjWWpPtC',
+    url: dotenv.env['SUPABASE_URL']!,
+    anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
   );
 
   runApp(const MyApp());
